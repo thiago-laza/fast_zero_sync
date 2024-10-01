b0d694ac73f9df8f5eece489dc537d98cdb7c0c4
thiago-laza <thiago.profmat@gmail.com>
2024-10-01 07:45:13 -0300

    Ajuste em tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,4 +13,4 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')  # Act(acao)
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
-    assert response.json() == {'message': 'Ola mundo3'}
+    assert response.json() == {'message': 'Ola mundo !!!'}
